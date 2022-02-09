8062a275785848bb491e30d4226d45628160e4f5
Tormod Flesjø <tormod.flesjo@gmail.com>
2022-02-09 14:20:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { CirclePicker } from "react-color";
 
 function App() {
+  const [color, setColor] = useState("#fff");
+  const style = { backgroundColor: color };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main style={style}>
+      <CirclePicker
+        color={color}
+        onChangeComplete={(color) => setColor(color.hex)}
+      />
+    </main>
   );
 }
 
