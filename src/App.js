113f826814b33c6719f02cfd54a14df428697c0f
Tormod Flesjø <tormod.flesjo@gmail.com>
2022-02-09 15:21:41 +0100

    add local storage to function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CirclePicker } from "react-color";
 
 function App() {
   const [color, setColor] = useState("#fff");
+  useEffect(() => {
+    const savedColor = localStorage.getItem("hex");
+    if (savedColor) {
+      setColor(savedColor);
+    }
+  }, []);
+
+  const handleColorChange = (color) => {
+    const { hex } = color;
+    setColor(hex);
+    localStorage.setItem("hex", hex);
+  };
+
   const style = { backgroundColor: color };
   return (
     <main style={style}>
-      <CirclePicker
-        color={color}
-        onChangeComplete={(color) => setColor(color.hex)}
-      />
+      <CirclePicker color={color} onChangeComplete={handleColorChange} />
     </main>
   );
 }
